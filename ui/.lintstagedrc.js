bd7c12c08e5654a6cbce2601808e74446567d80d
Rui <rzhengphy@gmail.com>
2023-04-03 12:58:13 +0800

    add prettier,commitlint,lint-staged

---
+++ ui/.lintstagedrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'pnpm exec tsc --noEmit --emitDeclarationOnly false',
+
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `pnpm exec eslint ${filenames.join(' ')}`,
+    `pnpm exec prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': (filenames) =>
+    `pnpm exec prettier --write ${filenames.join(' ')}`,
+};
