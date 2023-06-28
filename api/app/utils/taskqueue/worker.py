afe49dfd4b8f18161963f0e2157857328dc750ca
Rui <rzhengphy@gmail.com>
2023-06-28 17:56:20 +0800

    feat: workable default + custom api serving for formulas

--- api/app/utils/taskqueue/worker.py
+++ api/app/utils/taskqueue/worker.py
@@ -4,3 +4,5 @@ from ...config import CELERY_BACKEND, CELERY_BROKER, CELERY_INCLUDE
 
 Q_FORMULA = "formulaQ"
 appFormula = Celery("appFormula", backend=CELERY_BACKEND, broker=CELERY_BROKER, include=f"{CELERY_INCLUDE}.formula")
+
+appFormula.conf.update({"broker_connection_retry_on_startup": True})
